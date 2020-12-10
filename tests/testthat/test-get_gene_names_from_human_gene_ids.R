9f8d329194d67c1c0851903187f40f65bdb57989
richelbilderbeek <richel@richelbilderbeek.nl>
2020-12-10 12:46:57 +0100

    Depend on sprentrez package

--- tests/testthat/test-get_gene_names_from_human_gene_ids.R
+++ tests/testthat/test-get_gene_names_from_human_gene_ids.R
@@ -14,3 +14,14 @@ test_that("verbose", {
     )
   )
 })
+
+test_that("use too many", {
+  skip("WIP")
+  gene_ids <- readr::read_csv(
+    file = system.file("extdata", "all_gene_ids.csv", package = "ncbi"),
+    col_types = readr::cols(gene_id = readr::col_double())
+  )$gene_id
+  gene_names <- get_gene_names_from_human_gene_ids(gene_ids)
+  expected_gene_names <- c("EGFR", "TNF", "APOE", "TGFB1", "HIF1A", "IL10")
+  expect_equal(gene_names, expected_gene_names)
+})
